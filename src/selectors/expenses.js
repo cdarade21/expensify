2760457f7afb23d1f45ed3e76e1e3a0617e59884
Chaitanya Dnyaneshwar Darade <chaitanyadnyaneshwardarade@Chaitanyas-MacBook-Air.local>
2020-08-11 02:20:16 -0400

    AddExpenseSummary to DashBoard

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,23 +1,20 @@
-import moment from 'moment'
+import moment from 'moment';
 
-const getVisibleExpenses= (expenses, {text, sortBy, startDate, endDate}) => {
-    return expenses.filter((expense)=>{
-        const createdAtMoment = moment(expense.createdAt)
-        const startDateMatch =  startDate ? startDate.isSameOrBefore(createdAtMoment,'day') : true
-        //const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate
-        const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment,'day') : true
-        //const endDateMatch =  typeof endDate !== 'number' || expense.createdAt <= endDate
-        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase())
+// Get visible expenses
 
-        return startDateMatch && endDateMatch && textMatch
-    }).sort((a,b)=>{
-        if(sortBy==='date') {
-            return a.createdAt < b.createdAt ? 1 : -1
-        }
-        else if (sortBy==='amount') {
-            return a.amount < b.amount ? 1 : -1
-        }
-    })
-}
+export default (expenses, { text, sortBy, startDate, endDate }) => {
+  return expenses.filter((expense) => {
+    const createdAtMoment = moment(expense.createdAt);
+    const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
+    const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
-export default getVisibleExpenses
+    return startDateMatch && endDateMatch && textMatch;
+  }).sort((a, b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1;
+    } else if (sortBy === 'amount') {
+      return a.amount < b.amount ? 1 : -1;
+    }
+  });
+};
